7b641a44bb3c9aeeee6b0f08703182175cb4b566
Luis Ferrufino <a2020115239@estudiantes.upsa.edu.bo>
2023-03-10 00:08:08 -0400

    codigo introductorio y explicacion de instalacion g++

---
+++ intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "stdio.h"
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    cout<<"Hola mundo!\n";
+    cout<<"Ingresa un numero: ";
+    cin>>n;
+    cout<<"Tu numero es: "<<n<<endl<<"Gracias por participar!\n";
+
+//    system("pause");
+}
